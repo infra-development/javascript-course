11cad44c8fa64398fcdf7d230c1651d4f8b2670d
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2021-12-09 15:49:46 +0530

    JC-22 Logical Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -234,4 +234,23 @@ if (favourite === 23) {
 if (favourite !== 23) {
     console.log("")
 }
+*//*
+const hasDriversLicense = true; //A
+const hasGoodVision = true; //B
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision); 
+console.log(!hasDriversLicense);
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log("Shara is able to drive!");
+// } else {
+//     console.log("Shara is not able to drive \n\ someone else should drive...");
+// }
+const isTired = true;
+console.log(hasDriversLicense && hasGoodVision && isTired);
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log("Shara is able to drive!");
+} else {
+    console.log("Shara is not able to drive \n\ someone else should drive...");
+}
 */
