2db157249b32dc862bbe5471c314609928b6c748
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2021-12-11 07:13:27 +0530

    JC-32 Functions

--- 02-Fundamentals – Part-2/script.js
+++ 02-Fundamentals – Part-2/script.js
@@ -8,3 +8,27 @@ if (hasDriversLicense) console.log("I can drive : D");
 
 // const interface = 'Audio';
 // const private = 534;
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(4,2);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(5, 7);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
