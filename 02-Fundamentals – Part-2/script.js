12be8161ce60deb387dda6b702dcd1a0048b9818
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2021-12-27 07:15:13 +0530

    JC-42 Dot vs. Bracket Notation

--- 02-Fundamentals – Part-2/script.js
+++ 02-Fundamentals – Part-2/script.js
@@ -306,3 +306,38 @@ const kaushik = {
     job: 'teacher',
     friends: ['Michael', 'Peter', 'Steven']
 };
+
+//challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'schmedtman',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas)
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+const nameKey = 'Name';
+console.log(jonas['last' + nameKey]);
+console.log(jonas['first' + nameKey]);
+
+// console.log(jonas.'first' + namekey);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);    
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+//challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+console.log(`${jonas.firstName} has ${jonas['friends'].length} friends, and his best friend is called ${jonas['friends'][0]}`);
