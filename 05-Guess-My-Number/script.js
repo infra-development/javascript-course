b17132ef50d208558909ef15520985f24c44df98
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2022-01-14 15:56:07 +0530

    JC-72_11:40 Coding Challenge#1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -18,11 +18,11 @@ document.querySelector('.guess').value = 13;
 console.log(document.querySelector('.guess').value);
 */
 //for secret number ---> random method
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 //try to decreesing value of .score
-let scored = Number(document.querySelector('.score').textContent);
+// let scored = Number(document.querySelector('.score').textContent);
+let scored = 20;
 console.log(scored);
 //added event listener -on click event --> we set function for value-> |print in .message|decrease value of .score
 document.querySelector('.check').addEventListener('click', function () {
@@ -39,7 +39,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //when the player win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -66,4 +66,27 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
-// no1 = document.querySelector('.score').textContent;
+// JavaScript in the Browser: DOM and Events
+// Coding Challenge #1
+// Implement a game rest functionality, so that the player can make a new guess!
+// Your tasks:
+// 1. Select the element with the 'again' class and attach a click event handler
+// 2. In the handler function, restore initial values of the 'score' and
+// 'secretNumber' variables
+// 3. Restore the initial conditions of the message, number, score and guess input
+// fields
+// 4. Also restore the original background color (#222) and number width (15rem)
+// GOOD LUCK 😀
+// adding functionalty to restor in again property on click event
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1; //reset number
+  scored = 20;
+  console.log(secretNumber);
+  document.querySelector('.score').textContent = scored; //reset score
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+});
