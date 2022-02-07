f67e07f4cb1ae595808eee3c96bc94232aefef3b
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2022-02-08 02:10:17 +0530

    JC-98_19:46 Destructuring objects

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -11,13 +11,72 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, maincourseIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[maincourseIndex]];
   },
+  orderDelivery: function ({
+    starterIndex = 2,
+    mainIndex = 0,
+    address,
+    time = '22:30',
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
-const arr = [2, 3, 4];
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Gayatri nagar ,36',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+restaurant.orderDelivery({
+  address: 'near by Ambaji Temple',
+  starterIndex: 1,
+});
 
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+// if we wanted the variable name to be different from
+// the property names?
+// well, we can do it like this
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags); // it is use full when we're dealing with third party data
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+/*
+////////////////////////////////
+// Destructuring Arrays
+const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
@@ -51,3 +110,4 @@ console.log(i, j, k);
 // Default values
 const [o = 1, p = 1, q = 1] = [2, 9];
 console.log(o, p, q);
+*/
